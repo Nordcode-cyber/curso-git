139a4831e8e7127a64d82f65522446eb080abf2f
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2025-04-04 23:06:15 -0300

    merge.bash

---
+++ merge.bash
@@ -0,0 +1,31 @@
+Norberto@Desktop MINGW64 ~/curso-git (main)
+$ git merge graficos
+Updating 6a56aa3..3f6d571
+Fast-forward
+ Arbol-confirmacion.png        | Bin 0 -> 69401 bytes
+ README.md                     |  13 +
+ arbol-confirmacion.drawio.svg | 634 ++++++++++++++++++++++++++++++++++++++++++
+ branch.MD                     |  36 +++
+ branch.drawio.svg             |   1 +
+ commit.drawio.svg             |   2 +
+ confirmaciones.drawio.svg     |   2 +
+ curso-git.code-workspace      |   8 +
+ estados.MD                    |  33 +++
+ estados.drawio.svg            | 251 +++++++++++++++++
+ flujo-de-trabajo.MD           |  22 ++
+ flujo-de-trabajo.drawio.svg   |   2 +
+ merge.MD                      |   1 +
+ 13 files changed, 1005 insertions(+)
+ create mode 100644 Arbol-confirmacion.png
+ create mode 100644 README.md
+ create mode 100644 arbol-confirmacion.drawio.svg
+ create mode 100644 branch.MD
+ create mode 100644 branch.drawio.svg
+ create mode 100644 commit.drawio.svg
+ create mode 100644 confirmaciones.drawio.svg
+ create mode 100644 curso-git.code-workspace
+ create mode 100644 estados.MD
+ create mode 100644 estados.drawio.svg
+ create mode 100644 flujo-de-trabajo.MD
+ create mode 100644 flujo-de-trabajo.drawio.svg
+ create mode 100644 merge.MD
