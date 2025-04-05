227df625856bac32591f8f9bbaff6157d101bb38
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2025-04-04 23:12:16 -0300

    new file:   commit.bash

---
+++ commit.bash
@@ -0,0 +1,490 @@
+
+Norberto@Desktop MINGW64 ~/curso-git (main)
+$ git log
+commit 139a4831e8e7127a64d82f65522446eb080abf2f (HEAD -> main, origin/main)
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Fri Apr 4 23:06:15 2025 -0300
+
+    merge.bash
+
+commit a9fd1d236fd27f14ccdd671be62b73626426a37e
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Fri Apr 4 23:05:35 2025 -0300
+
+    new file:   merge.bash
+
+commit 5d648a9fa61478c3caf1c2fb1f58154d2668a71a
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Fri Apr 4 22:55:35 2025 -0300
+
+    new file:   confirmacionlectura.png
+
+commit 83419c413e075ff3cc442e4f83c7aab0f4828a97
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Fri Apr 4 22:45:28 2025 -0300
+
+    update confirmaciones.drawio.svg
+
+commit 42941decdf07f6d94ca4c8c7e12ac61ca0b6bd64
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Fri Apr 4 22:20:06 2025 -0300
+
+    update  confirmaciones.drawio.svg
+
+commit efde3940309cd53524fbd8c5f8666a7ea6bbf198
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Fri Apr 4 22:10:24 2025 -0300
+
+    modified:   confirmaciones.drawio.svg
+:
+commit 139a4831e8e7127a64d82f65522446eb080abf2f (HEAD -> main, origin/main)
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Fri Apr 4 23:06:15 2025 -0300
+
+    merge.bash
+
+commit a9fd1d236fd27f14ccdd671be62b73626426a37e
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Fri Apr 4 23:05:35 2025 -0300
+
+    new file:   merge.bash
+
+commit 5d648a9fa61478c3caf1c2fb1f58154d2668a71a
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Fri Apr 4 22:55:35 2025 -0300
+
+    new file:   confirmacionlectura.png
+
+commit 83419c413e075ff3cc442e4f83c7aab0f4828a97
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Fri Apr 4 22:45:28 2025 -0300
+
+    update confirmaciones.drawio.svg
+
+commit 42941decdf07f6d94ca4c8c7e12ac61ca0b6bd64
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Fri Apr 4 22:20:06 2025 -0300
+
+    update  confirmaciones.drawio.svg
+commit 139a4831e8e7127a64d82f65522446eb080abf2f (HEAD -> main, origin/main)
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Fri Apr 4 23:06:15 2025 -0300
+
+    merge.bash
+
+commit a9fd1d236fd27f14ccdd671be62b73626426a37e
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Fri Apr 4 23:05:35 2025 -0300
+
+    new file:   merge.bash
+
+commit 5d648a9fa61478c3caf1c2fb1f58154d2668a71a
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Fri Apr 4 22:55:35 2025 -0300
+
+    new file:   confirmacionlectura.png
+
+commit 83419c413e075ff3cc442e4f83c7aab0f4828a97
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Fri Apr 4 22:45:28 2025 -0300
+
+    update confirmaciones.drawio.svg
+
+commit 42941decdf07f6d94ca4c8c7e12ac61ca0b6bd64
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Fri Apr 4 22:20:06 2025 -0300
+
+    update  confirmaciones.drawio.svg
+
+commit efde3940309cd53524fbd8c5f8666a7ea6bbf198
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Fri Apr 4 22:10:24 2025 -0300
+
+    modified:   confirmaciones.drawio.svg
+
+commit 878aae25aca82ab773556d787a7a04e6d598ced4
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Fri Apr 4 22:09:42 2025 -0300
+
+    new file:   branch.png
+
+commit 139a4831e8e7127a64d82f65522446eb080abf2f (HEAD -> main, origin/main)
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Fri Apr 4 23:06:15 2025 -0300
+
+    merge.bash
+
+commit a9fd1d236fd27f14ccdd671be62b73626426a37e
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Fri Apr 4 23:05:35 2025 -0300
+
+    new file:   merge.bash
+
+commit 5d648a9fa61478c3caf1c2fb1f58154d2668a71a
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Fri Apr 4 22:55:35 2025 -0300
+
+    new file:   confirmacionlectura.png
+
+commit 83419c413e075ff3cc442e4f83c7aab0f4828a97
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Fri Apr 4 22:45:28 2025 -0300
+
+    update confirmaciones.drawio.svg
+
+commit 42941decdf07f6d94ca4c8c7e12ac61ca0b6bd64
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Fri Apr 4 22:20:06 2025 -0300
+
+    update  confirmaciones.drawio.svg
+
+commit efde3940309cd53524fbd8c5f8666a7ea6bbf198
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Fri Apr 4 22:10:24 2025 -0300
+
+    modified:   confirmaciones.drawio.svg
+
+commit 878aae25aca82ab773556d787a7a04e6d598ced4
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Fri Apr 4 22:09:42 2025 -0300
+
+    new file:   branch.png
+
+commit 60fa41d50b47feb92956223c6fdd43415ca6ecf9
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Fri Apr 4 21:42:57 2025 -0300
+
+    output Gitbash merge
+
+commit 3f6d5719d6fc69e7ac3f001a8e24995011880995 (origin/graficos, origin/HEAD)
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Fri Apr 4 21:33:03 2025 -0300
+
+    update changes svg
+:...skipping...
+commit 139a4831e8e7127a64d82f65522446eb080abf2f (HEAD -> main, origin/main)
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Fri Apr 4 23:06:15 2025 -0300
+
+    merge.bash
+
+commit a9fd1d236fd27f14ccdd671be62b73626426a37e
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Fri Apr 4 23:05:35 2025 -0300
+
+    new file:   merge.bash
+
+commit 5d648a9fa61478c3caf1c2fb1f58154d2668a71a
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Fri Apr 4 22:55:35 2025 -0300
+
+    new file:   confirmacionlectura.png
+
+commit 83419c413e075ff3cc442e4f83c7aab0f4828a97
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Fri Apr 4 22:45:28 2025 -0300
+
+    update confirmaciones.drawio.svg
+
+commit 42941decdf07f6d94ca4c8c7e12ac61ca0b6bd64
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Fri Apr 4 22:20:06 2025 -0300
+
+    update  confirmaciones.drawio.svg
+
+commit efde3940309cd53524fbd8c5f8666a7ea6bbf198
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Fri Apr 4 22:10:24 2025 -0300
+
+    modified:   confirmaciones.drawio.svg
+
+commit 878aae25aca82ab773556d787a7a04e6d598ced4
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Fri Apr 4 22:09:42 2025 -0300
+
+    new file:   branch.png
+
+commit 60fa41d50b47feb92956223c6fdd43415ca6ecf9
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Fri Apr 4 21:42:57 2025 -0300
+
+    output Gitbash merge
+
+commit 3f6d5719d6fc69e7ac3f001a8e24995011880995 (origin/graficos, origin/HEAD)
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Fri Apr 4 21:33:03 2025 -0300
+
+    update changes svg
+
+commit ae673faad68964c5919136ee6146ca847204a2d6
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Fri Apr 4 20:25:56 2025 -0300
+
+    update merge.MD & flujo-de-trabajo.MD
+
+commit 0c0254cacf4b820405970635b4fc7ef5185145b4
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Mon Mar 24 22:38:26 2025 -0300
+
+    update flujo-de-trabajo.MD
+
+commit 593fb7cc765190e781412f21c18689edc7522b84
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Mon Mar 24 22:33:50 2025 -0300
+
+    new file:   estados.drawio.svg
+
+commit 437f9964a0c3b5c8717bf72b6d3bc100fdca7b0a
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Mon Mar 24 22:30:31 2025 -0300
+
+    conversion a SVG estados.drawio
+
+commit a5ce869470e2f5bdc8130390bf40a2c26aba62e0
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Mon Mar 24 21:46:17 2025 -0300
+
+    new file:   estados.drawio
+
+commit 2118276b24455b0be8abe465903467d063f878ba
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Mon Mar 24 21:39:43 2025 -0300
+
+    flujo-de-trabajo se convierte a SVG
+
+commit 5a56f0fe9021de9d5275ee029ab7896404fdf9bc
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Mon Mar 24 21:11:50 2025 -0300
+
+    new file:   flujo-de-trabajo.drawio
+
+commit 3eb4bcf6f072a93fe2f28a575ff31ad0ab5333c5
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Mon Mar 24 21:05:38 2025 -0300
+
+    flujo de trabajo
+
+commit b756e97c0a10c7d36647ee09d0c4d7aafd8c4317
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Mon Mar 24 20:49:01 2025 -0300
+
+    new file:   estados.MD
+
+commit 1faf5e9dd7c61518ece453dce807530d07f1c2bc
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Mon Mar 24 15:29:31 2025 -0300
+
+    editamos confirmaciones.drawio.svg
+
+commit 3fa6d59fa765fe7814909621b4fe2b5a66f6599a
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Mon Mar 24 15:27:16 2025 -0300
+
+    borramos confirmaciones.drawio y creamos
+    confirmaciones.drawio.svg
+
+commit 846a8c5f38e7d9e950d28dae27c0d7a21f809338
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Mon Mar 24 15:24:27 2025 -0300
+
+    modified:   confirmaciones.drawio
+
+commit 972b65790d44e8ba65bc45b44a27eb9c0f36ac5b
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Mon Mar 24 15:10:32 2025 -0300
+
+    new file:   confirmaciones.drawio
+
+commit 960c760d322a0b25e418e3fbeb3915ce45ec6d84
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Mon Mar 24 15:05:41 2025 -0300
+
+    new file:   Arbol-confirmacion.png
+
+commit 0785a8d09b6619dda3ff9c61c312247de78db617
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Mon Mar 24 14:57:58 2025 -0300
+
+    Commit arbol de confirmacion
+
+commit fa6216d7bd6bdc3fe00cbb25ac93c7ca51d25b53
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Sun Mar 23 23:43:47 2025 -0300
+
+    conversion
+
+commit 764f226b9255222a1f0445ca225bad676e5b3c6f
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Sun Mar 23 23:43:04 2025 -0300
+
+    conversion de DRAWIO a SVG
+
+commit a8e88202e6e6f795159d0d17fcb9892db235b6a1
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Sun Mar 23 23:41:31 2025 -0300
+
+    update arbol-confirmacion
+
+commit debde783e4dd096237f1583738bad5fe68dfcb9b
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Sun Mar 23 23:21:13 2025 -0300
+
+    grafico arbol lista de contenido
+
+commit 6764b1f109e3468b889fdeb0ca1ba2422c33ea2b
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Sun Mar 23 23:13:19 2025 -0300
+
+    cierre branch.MD
+
+commit e87a1314047ab09159bda942865c52349d87a2af
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Sun Mar 23 23:01:54 2025 -0300
+
+    update branch.MD
+
+commit 3f4242c94127fc809646a53972c68fa5463ef684
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Sun Mar 23 22:56:29 2025 -0300
+
+    ramificaciones
+
+commit 87841f4ce09572e7d0f8f834ce82bb5dc1788f6c
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Sun Mar 23 22:49:28 2025 -0300
+
+    probando colores en markdown
+
+commit 2cfd451602866b8b0d472a435d26c4854ec2e848
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Sun Mar 23 22:40:25 2025 -0300
+
+    Que es una rama
+
+commit 5759b3ad7b0c0a18514ee2cd7ad7873e9f4d2760
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Sun Mar 23 22:08:56 2025 -0300
+
+    update branch.MD
+
+commit 486033dd8e8cb21a756b42b3b7f4362c36b7c510
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Sun Mar 23 21:49:37 2025 -0300
+
+    UPDATE README.md
+
+commit 975a0982721cd59e93183d5f38f8e34e464ef8ef
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Sun Mar 23 21:47:31 2025 -0300
+
+    update README.md
+
+commit ab17564117277d03e0c46229f66298b8308b4ae7
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Sun Mar 23 21:35:17 2025 -0300
+
+    README.md
+
+commit 08e73dbccc9402ef0dc41083895bb63d90a5899a
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Sun Mar 23 21:22:37 2025 -0300
+
+    flujo-de-trabajo.MD
+
+commit 0ad5b44de263525be268159b126e8d899089664b
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Sun Mar 23 16:31:26 2025 -0300
+
+    creamos merge.MD
+
+commit 63cfe9e2f6483baa9846a793f8843dc50b491964
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Sun Mar 23 16:08:48 2025 -0300
+
+    agregamos README.md al repositorio
+
+commit 4d420e826957372721d3238d632dc9eb8df4d70c
+Author: Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
+Date:   Sun Mar 23 15:53:24 2025 -0300
+
+    agregamos workspace vs code curso-git
+
+commit e1f1356c164311e9aaae0705f44639a14aaadb37
+Author: Norberto <norberto.contreras@gmail.com>
+Date:   Mon Mar 17 23:01:37 2025 -0300
+
+    borramos branch.drawio y creamos branch .draw.svg
+
+commit a3a2a06e0bbe3d75b834decbd65602ddaa0f2410
+Author: Norberto <norberto.contreras@gmail.com>
+Date:   Mon Mar 17 22:35:25 2025 -0300
+
+    branch.drawio
+
+commit 87d663a092b0f2c588e2ed3c7f9df9474d6c14b2
+Author: Norberto <norberto.contreras@gmail.com>
+Date:   Mon Mar 17 22:33:34 2025 -0300
+
+    commit.drawio.svg
+
+commit 5ba04b2fdaff0ecba678dd479ba508460511f5fd
+Author: Norberto <norberto.contreras@gmail.com>
+Date:   Mon Mar 17 22:24:43 2025 -0300
+
+    Agrgamos info en formato MarkDown
+
+commit 3128dd73c360a48a8764aeb33f56cd94df45a362
+Author: Norberto <norberto.contreras@gmail.com>
+Date:   Mon Mar 17 22:21:06 2025 -0300
+
+    Agregamos grafico commint de drawio
+
+commit 6a56aa387e407294ab7f446a667e88d162251ca1
+Author: Norberto <norberto.contreras@gmail.com>
+Date:   Mon Mar 17 21:52:48 2025 -0300
+
+    se elimino archivo txt
+
+commit 7d7043840aa414dfd5a556ca187501f864859792
+Merge: a0d977a 44ff5b3
+Author: Norberto <norberto.contreras@gmail.com>
+Date:   Mon Mar 17 21:35:25 2025 -0300
+
+    Merge branch 'version-js' fusion de rama
+
+commit a0d977a4e14a1ef7bfff76adf621b794ee941ddb
+Author: Norberto <norberto.contreras@gmail.com>
+Date:   Sun Mar 16 22:11:15 2025 -0300
+
+    texto expandido
+
+commit 44ff5b31ca54c63a3e73db4aae5f07cd815f05ed
+Author: Norberto <norberto.contreras@gmail.com>
+Date:   Sun Mar 16 17:09:38 2025 -0300
+
+    cambio de funcion en el metodo
+
+commit 372fc9960affb7a5e0f9a0496e41b950580722fd
+Author: Norberto <norberto.contreras@gmail.com>
+Date:   Sat Mar 15 17:48:19 2025 -0300
+
+    copiar info sobre commit
+
+commit 14de3b3384c637431923081eefa5f830d184a9d9
+Author: Norberto <norberto.contreras@gmail.com>
+Date:   Sat Mar 15 17:39:51 2025 -0300
+
+    corregir texto
+
+commit bf1e487bbf3cd1b47eb6bfc03d6c29324eff75b9
+Author: Norberto <norberto.contreras@gmail.com>
+Date:   Sat Mar 15 14:21:52 2025 -0300
+
+    cambiar palabra mundo por FreeCodeCamp
+
+commit 3a66f0cdd33dd116145a80ed2f98e8302a9f6f90
+Author: Norberto <norberto.contreras@gmail.com>
+Date:   Sat Mar 15 14:06:21 2025 -0300
+
+    agregar archivo de texto
+(END)
