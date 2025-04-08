6d81e26dc4eba1f123219173be2118034c71a723
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2025-04-07 22:22:05 -0300

    Update comandos.bash

--- comandos.bash
+++ comandos.bash
@@ -1,2 +1,17 @@
 #Comandos en Git Bash
-# Inicializar Git 
+# Inicializar new Git Repository en actual directorio
+$ mkdir railsapp
+$ cd railsapp
+$ git init
+#Realizar cambios en el directorio actual
+$ echo "# RAILS APP" > README.md
+$ echo "Demo >> README.MD"
+$ git add README.md 
+#confirma los cambios por etapas con un mensaje de confirmación
+$ git add app.java
+$ git commit -m "commit inicial"
+#confirmar los cambios de un repositorio local a uno remoto
+$ git add .
+$ git commit -m "Update files"
+$ git push origin main
+
